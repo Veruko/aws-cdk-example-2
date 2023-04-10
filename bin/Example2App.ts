f92b084b5f3b0fd0c7c1673dba3a67d998fbde48
Martynka <martynka@DESKTOP-HOME.localdomain>
2023-04-10 16:19:50 +0100

    Creating empty stacks

---
+++ bin/Example2App.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { PersistenceStack } from '../lib/PersistenceStack';
+import { GraphQlStack } from '../lib/GraphQlStack';
+
+const app = new cdk.App();
+
+const persistence = new PersistenceStack(app, 'PersistenceStack', {
+  stackName: "Persistence",
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
+const graphQl = new GraphQlStack(app, 'GraphQlStack', {
+  stackName: "GraphQl",
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
---
+++ functions/Example1Lambda.ts
@@ -0,0 +1,29 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { Handler } from "aws-lambda";
+
+type HelloEvent = {
+    name : string
+}
+
+const dynamoDb =  DynamoDBDocument.from(new DynamoDB({}));
+
+export const handler: Handler<HelloEvent, string> = async (event) => {
+  console.log("event", JSON.stringify(event));
+  
+  let nickname = event.name;
+
+  const response = await dynamoDb.get({
+    TableName: "example-1",
+    Key: {
+      name: event.name
+    }
+  });
+  console.log("🚀 ~ file: Example1Lambda.ts:22 ~ consthandler:Handler<HelloEvent,string>= ~ response:", response)
+
+  if (response && response.Item) {
+    nickname = response.Item["nickname"] ?? event.name
+  }
+
+  return `Hello ${nickname}!`;
+};
